40fee992388de163dfe6b834c69720a887e63eaf
Muhammad Haris Ahsan <harismirza3456@gmail.com>
2024-06-02 23:12:15 +0500

    applied proxy in vite-config file

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,12 @@ import axios from "axios";
 function App() {
   const [childInfo, setChildInfo] = useState([]);
 
-  const baseUrl = import.meta.VITE_BASE_URL || "http://localhost:3000";
+  // const baseUrl = import.meta.VITE_BASE_URL || "http://localhost:3000";
 
   useEffect(() => {
     axios
-      .get(`${baseUrl}/api/childinfo`)
+      // .get(`${baseUrl}/api/childinfo`)
+      .get(`/api/childinfo`)
       .then((res) => {
         setChildInfo(res.data);
         // console.log(res.data);
@@ -16,19 +17,29 @@ function App() {
       .catch((err) => {
         console.log(err);
       });
-  }, [baseUrl]);
+  }, []);
+
+  const p = { padding: 10 };
 
   return (
     <>
       <h1>Child Info</h1>
-      {childInfo.map((child, i) => {
-        return (
-          <div key={child.id || i}>
-            <h2>{child.name}</h2>
-            <p>{child.year}</p>
-          </div>
-        );
-      })}
+      <table border="1">
+        <thead>
+          <tr>
+            <th style={p}>Name</th>
+            <th style={p}>Year</th>
+          </tr>
+        </thead>
+        <tbody>
+          {childInfo.map((child) => (
+            <tr key={child.id}>
+              <td style={p}>{child.name}</td>
+              <td style={p}>{child.year}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   );
 }
